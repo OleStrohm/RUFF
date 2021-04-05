bad26e62ed056509a7fcfb1c0051ade5b33d39c2
Ole Strohm <strohm99@gmail.com>
2021-04-05 21:15:54 +0100

    Updated tb

--- cpu/cpu.v
+++ cpu/cpu.v
@@ -1,12 +1,11 @@
 module cpu (
-    input clk,
-    output var [31:0] instr_addr
+    input var clk
 );
-
-    initial instr_addr = 0;
+    bit [7:0] onchip_mem [0:32'h1000 - 1] /*verilator public*/;
+    logic [31:0] instr_addr /*verilator public*/ = 0; 
 
     always_ff @ (posedge clk)
-        instr_addr <= instr_addr + 1;
+        instr_addr <= instr_addr + 4;
 
 endmodule
 
--- cpu/tb.cpp
+++ cpu/tb.cpp
@@ -1,30 +1,60 @@
-#include <bits/stdint-uintn.h>
-#include <stdio.h>
-#include <stdint.h>
 #include "Vcpu.h"
+#include "Vcpu_cpu.h"
 #include "verilated.h"
+#include <fstream>
+#include <ios>
+#include <stdint.h>
+#include <stdio.h>
+#include <type_traits>
 
-void step(Vcpu* top, uint64_t &time);
+void step(Vcpu *top, uint64_t &time);
+#define INIT_ROM(rom) init_rom<std::extent<decltype(rom)>::value>(rom);
+#define QWORD(loc)                                                             \
+  ((cpu->onchip_mem[loc + 3] << 24) | (cpu->onchip_mem[loc + 2] << 16) |       \
+   (cpu->onchip_mem[loc + 1] << 8) | (cpu->onchip_mem[loc]))
 
-int main(int argc, char** argv, char** env) {
-    Verilated::commandArgs(argc, argv);
-    Vcpu* top = new Vcpu;
-    uint64_t time = 0;
-    top->clk = 0;
+template <unsigned int N> void init_rom(CData mem[N]) {
+  std::ifstream rom("rom.bin", std::ios::in | std::ios::binary);
 
-    while (!Verilated::gotFinish() && time < 10) {
-        step(top, time);
-        printf("instr_addr: %08X\n", top->instr_addr);
-    }
+  char buffer[N];
+  for (int i = 0; i < N; i++) {
+    buffer[i] = 0xFF;
+  }
 
-    delete top;
-    return 0;
+  if (rom.is_open()) {
+    rom.seekg(0, std::ios::beg);
+    rom.read(buffer, N);
+  }
+
+  for (int i = 0; i < N; i++) {
+    mem[i] = buffer[i];
+  }
+
+  rom.close();
 }
 
-void step(Vcpu* top, uint64_t &time) {
-    top->clk = 0;
-    top->eval();
-    top->clk = 1;
-    top->eval();
-    time++;
+int main(int argc, char **argv, char **env) {
+  Verilated::commandArgs(argc, argv);
+  auto top = new Vcpu;
+  auto cpu = top->cpu;
+  uint64_t time = 0;
+  top->clk = 0;
+
+  INIT_ROM(cpu->onchip_mem);
+
+  while (!Verilated::gotFinish() && time < 10) {
+    printf("rip: %08X\tinstr:%08X\n", cpu->instr_addr, QWORD(cpu->instr_addr));
+    step(top, time);
+  }
+
+  delete top;
+  return 0;
+}
+
+void step(Vcpu *top, uint64_t &time) {
+  top->clk = 0;
+  top->eval();
+  top->clk = 1;
+  top->eval();
+  time++;
 }
